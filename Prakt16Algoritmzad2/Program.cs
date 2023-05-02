43acc11361068d1e3e4f0b839b4e0b7676635809
Avondeil <123551851+Avondeil@users.noreply.github.com>
2023-05-03 00:05:45 +0500

    Add files via upload

---
+++ Prakt16Algoritmzad2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt16Algoritmzad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Введите элементы массива с использованием (/): ");
+            string[] text = Console.ReadLine().Split('/');
+            foreach (string str in text)
+            {
+                foreach (char numb in str)
+                {
+                    if (char.IsDigit(numb)) { count++; }
+                }
+            }
+            Console.WriteLine($"Кол-во цифр в массиве: {count}");
+            Console.WriteLine($"Элементы массива до символа (/): ");
+            foreach (string str in text)
+            {
+                if (str == "/")
+                {
+                    break;
+                }
+                if (str != "") 
+                { 
+                    Console.WriteLine(str); 
+                }
+            }
+            Console.WriteLine($"Элементы массива после символа (/) в верхнем и нижнем регистрах:");
+            foreach (string str in text)
+            {
+                string info = "";
+                if (str == "/") { continue; }
+                if (str == "") { break; }
+                foreach (char znak in str)
+                {
+                    if (Char.IsUpper(znak))
+                    {
+                        info += Char.ToLower(znak);
+                    }
+                    else if (Char.IsLower(znak))
+                    {
+                        info += Char.ToUpper(znak);
+                    }
+                    else { info += znak; }
+                }
+                Console.WriteLine(info);
+                StreamWriter st = new StreamWriter("file.txt", true);
+                st.WriteLine(info);
+                st.Close();
+            }
+            Console.WriteLine("Измененный массив был записан в файл");
+            Console.ReadKey();
+        }
+    }
+}
